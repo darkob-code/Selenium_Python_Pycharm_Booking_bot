396c10be1bf3d40b48019f1502adf726f3e9e3aa
darkob-code <87665820+darkob-code@users.noreply.github.com>
2023-02-05 14:56:03 +0100

    Add files via upload

---
+++ booking/booking.py
@@ -0,0 +1,13 @@
+import booking.constants as const
+import os
+from selenium import  webdriver
+
+
+class Booking(webdriver.Chrome):
+    def __int__(self, driver_path=r"C:\Selenium_Driver"):
+        self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
+        super(Booking, self).__init__()
+
+    def land_first_page(self):
+        self.get(const.BASE_URL)
